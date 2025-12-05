19f606e39656e95580470413083ad9ac00cc0080
Tom De Dobbeleer <tdedobbeleer@gmail.com>
2025-12-05 17:58:32 +0100

    firebase error fix

--- firebase.ts
+++ firebase.ts
@@ -3,9 +3,9 @@ import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4yG_GAL-aYJ-OVqC-yDssMfyDGoWdaUQ",
-  authDomain: "your-prod-project.firebaseapp.com",
+  authDomain: "carpolly-4fe11.firebaseapp.com",
   projectId: "carpolly-4fe11",
-  storageBucket: "your-prod-project.appspot.com",
+  storageBucket: "carpolly-4fe11.appspot.com",
   messagingSenderId: "611172017575",
   appId: "1:611172017575:web:f0b52a8ace683ec19f8282"
 }
